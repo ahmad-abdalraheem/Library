b559209dcfc08bd1d0bdb13ffa7f6e8ed5f78299
Ahmad Abdalraheem <ahmad.abdalraheem@optimumpartners.co>
2024-08-13 16:49:50 +0300

    Initialize Domain Layer

---
+++ Domain/FileHandler/IBookHandler.cs
@@ -0,0 +1,9 @@
+using Domain.Entities;
+
+namespace Domain.FileHandler;
+
+public interface IBookHandler
+{
+	public bool Write(List<Book> books);
+	public List<Book>? Read();
+}
---
+++ Domain/Repository/IBookRepository.cs
@@ -0,0 +1,12 @@
+using Domain.Entities;
+
+namespace Domain.Repository;
+
+public interface IBookRepository
+{
+	public bool Add(Book book);
+	public bool Update(Book? book);
+	public bool Delete(int bookId);
+	public List<Book?>? Get();
+	public Book? GetById(int bookId);
+}
